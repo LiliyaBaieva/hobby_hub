3ed7b988b2f65b9fef5dedb0e959a8d48c8df208
LiliyaBaieva <555blv@gmail.com>
2026-02-27 12:28:31 +0100

    Realise earch und filtation

--- school_admin/admin.py
+++ school_admin/admin.py
@@ -1,17 +1,38 @@
 from django.contrib import admin
 from .models import Teacher, ExtraLesson, Student
 
+# Налаштування заголовків (Завдання 1)
+admin.site.site_header = "Система управління HobbyHub"
+admin.site.site_title = "Адміністратор HobbyHub"
+admin.site.index_title = "Панель керування гуртками та учнями"
+
+# Інлайн для учнів у гуртках (Завдання 4)
+class StudentInline(admin.TabularInline):
+    model = Student
+    extra = 1
+
+# Власна дія (Завдання 5)
+@admin.action(description="Оновити вибрані записи")
+def custom_refresh(modeladmin, request, queryset):
+    queryset.update()
+    modeladmin.message_user(request, "Дані успішно оновлено.")
+
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'middle_name')
-    search_fields = ('last_name',)
+    search_fields = ('last_name', 'first_name')
+    list_filter = ('last_name',)
 
 @admin.register(ExtraLesson)
 class ExtraLessonAdmin(admin.ModelAdmin):
     list_display = ('name', 'teacher')
     list_filter = ('teacher',)
+    search_fields = ('name',)
+    inlines = [StudentInline]
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'school_class', 'lesson')
-    list_filter = ('school_class', 'lesson')
+    list_display = ('full_name', 'lesson', 'school_class', 'age')
+    list_filter = ('lesson', 'school_class')
+    search_fields = ('full_name',)
+    actions = [custom_refresh]
